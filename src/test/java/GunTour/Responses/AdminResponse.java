ec875f239c05de4b65e394f334c56ec2a4d74f98
Bunga Ayu Ferdiyanti <bungaayu9599@gmail.com>
2022-11-10 11:44:22 +0700

    Product, Ranger, Claimber by Admin Feature

--- src/test/java/GunTour/Responses/AdminResponse.java
+++ src/test/java/GunTour/Responses/AdminResponse.java
@@ -2,7 +2,6 @@ package GunTour.Responses;
 
 public class AdminResponse {
     public static String MESSAGE = "message";
-    public static String MESSAGE_RANGER = "data.message";
     public static String PAGE = "page";
     public static String ID = "id";
 }
